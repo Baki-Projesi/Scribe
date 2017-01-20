59f2743a04efb71c78c67feb27c86caa15999515
Brad Holland <bholland.mail@gmail.com>
2017-01-19 23:41:39 -0800

    work on outputBox & rules

--- src/components/OutputBox.js
+++ src/components/OutputBox.js
@@ -1,6 +1,45 @@
 import React, { Component } from 'react';
-
+import { ottomanArabic, ottomanTurkish, ottomanLatin, ottomanCyrillic } from '../utils/reverse-rules'
 class OutputBox extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            language: "ottomanTurkish", //default to Turkish
+            displayText: "Nothing to translate"
+        }
+    }
+
+    translate(input) {
+        const language = this.state.language;
+        const inputText = input;
+        var outputText;
+        var translationRules;
+
+        if (language === "ottomanTurkish") {
+            translationRules = ottomanTurkish;
+        }
+        else if (language == "ottomanArabic") {
+            translationRules = ottomanArabic;
+        }
+        else if (language === "ottomanLatin") {
+            translationRules = ottomanLatin;
+        }
+        else if (language === "ottomanCyrillic") {
+            translationRules = ottomanCyrillic;
+        }
+        
+        for (var i = 0; i < inputText.length; ++i) {
+            let character = inputText.charAt(i);
+            
+        }
+    }
+
+    onLanguageChange(language) {
+        if (language) {
+            this.state.language = language;
+        }
+    }
 
     render() {
 
---
+++ src/utils/rules.js
@@ -0,0 +1,27 @@
+
+//Exports the following rules objects in the format
+// inputLetter: OttomanCode
+
+//Exported objects are as follows:
+// { arabicCodes, turkishCodes, latinCodes, cyrillicCodes }
+
+// 1. Arabic: OttomanCode
+// 2. Turkish: OttomanCode
+// 3. Latin: OttomanCode 
+// 4. Cyrillic: OttomanCode
+
+export const arabicCodes = {
+
+}
+
+export const turkishCodes = {
+
+} 
+
+export const latinCodes = {
+    
+}
+
+export const cyrillicCodes = {
+
+}
