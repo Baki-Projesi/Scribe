dfa2fe79dab77a4f04fc8d5642257dc2fc760382
Rutvi Patel <rutvimpatel@gmail.com>
2017-01-19 22:30:52 -0800

    did noting

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React </h2>
-          <p className="App-intro">
-          </p>
+          <p className="App-intro"></p>
         </div>
       </div>
     );
