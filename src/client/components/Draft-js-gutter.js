d70f23d0e6d94a002a0778e82f768df63dc8495c
Rutvi Patel <rutvimpatel@gmail.com>
2017-05-31 14:13:48 -0700

    deleted extra code

--- src/client/components/Draft-js-gutter.js
+++ src/client/components/Draft-js-gutter.js
@@ -14,28 +14,15 @@ export class EditorGutter extends Component {
   constructor(props) {
     super(props);
     this.state = {editorState: this.props.editorState || EditorState.createEmpty()};
-
-    var s1 = document.getElementById('scrolling1');
-    var s2 = document.getElementById('scrolling2');
   }
 
-    select_scroll_1(e) { this.s2.scrollTop = this.s1.scrollTop; }
-    select_scroll_2(e) { this.s1.scrollTop = this.s2.scrollTop; }
-
   render() {
-    if(this.s1 != null && this.s2 !=null) {
-        this.s1.addEventListener('scroll', this.select_scroll_1, false);
-        this.s2.addEventListener('scroll', this.select_scroll_2, false);
-        console.log("event listners added")
-    }
-
     return (
         
       <div id = "inputcontainer" style={Object.assign(this.props.style, {
           display: 'flex'
         })}>
         <ol 
-            id="scrolling1"
             {...this.props.list}
             style={Object.assign(this.props.styleList, {
             margin: 0,
@@ -50,7 +37,6 @@ export class EditorGutter extends Component {
           )}
         </ol>
         <div 
-            id="scrolling2"
             style={{flex:1}} >
           <Editor {...this.props.editor}
             style={this.props.styleEditor}
